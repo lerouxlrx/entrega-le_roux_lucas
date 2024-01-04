bf1427a5a4f3554ea98962df35806038d9dfaa25
DevLLR <149336208+DevLLR@users.noreply.github.com>
2024-01-03 23:22:54 -0300

    Funcionalidad btnEliminar

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,6 +1,13 @@
 import './CartItem.css'
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
 
 const CartItem = ({id, name, price, quantity}) => {
+    const { removeItem } = useContext(CartContext);
+    const handleRemoveClick = () => {
+        removeItem(id);
+      };
+
     return (
         <article className="CartItem">
             <div className="CartHeader">
@@ -23,7 +30,7 @@ const CartItem = ({id, name, price, quantity}) => {
                     </p>
                 </section>
                 <section className="EliminarItem">
-                    <button className="BtnEliminarItem">
+                    <button className="BtnEliminarItem" onClick={handleRemoveClick}>
                         Eliminar
                     </button>
                 </section>
